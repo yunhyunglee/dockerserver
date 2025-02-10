abbf7a3a421e82e998eb1f9fc8c9645d95dc6b4b
fbdlsgud <dlsgud4865@gmail.com>
2025-02-10 18:59:56 +0900

    fix: home.jsx 수정(안뜨다가 뜸;)

--- TeamDive_Client/src/components/frame/home.jsx
+++ TeamDive_Client/src/components/frame/home.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import styles from '../../css/main.module.css';
-import { LoginPage } from './LoginPage';
+import AppRoutes from '../../routes';
 
 function Home({ menubar }) {
     return (
@@ -21,17 +21,8 @@ function Home({ menubar }) {
                 </div>
             </div>
 
-            <div className={`${styles.main} ${menubar ? styles.move : ''}`}>
-                <Routes>
-                    <Route path='/' element={<h4>메인페이지임</h4>} />
-                    <Route path='/today' element={<h4>오늘의 인기차트 페이지임</h4>} />
-                    <Route path='/playList' element={<h4>플리페이지임</h4>} />
-                    <Route path='/membership' element={<h4>멤버십 페이지임</h4>} />
-                    <Route path='/menu2' element={<h4>메뉴2 페이지임</h4>} />
-                    <Route path='/menu3' element={<h4>메뉴3 페이지임</h4>} />
-                    <Route path='/login' element={<LoginPage></LoginPage>} />
-                    <Route path='/sign-up' element={<h4>회원가입 페이지임</h4>} />
-                </Routes>
+            <div className={`${styles.main} ${menubar ? styles.move : ""}`}>
+                <AppRoutes />  
             </div>
         </div>
     );
