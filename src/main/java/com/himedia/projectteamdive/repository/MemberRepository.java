4e494fbf65b394e9b1febdbad89d42e850b5103a
NG <hng8907@gmail.com>
2025-02-10 22:40:15 +0900

    feat: login 구현, fix: redux 구현을 위해  ./src/main.jsx 바꿈

--- src/main/java/com/himedia/projectteamdive/repository/MemberRepository.java
+++ src/main/java/com/himedia/projectteamdive/repository/MemberRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, String> {
 
 
-    Member findBymemberId(String memberId);
+    Member findByMemberId(String memberId);
 }
