926d81cb3c2e5775dec1582f942899d64a9dcb83
hana <o3o_s2__@naver.com>
2025-02-11 14:59:53 +0900

    feat: membership Entity 필드 추가 및 카테고리에 따른 멤버십 조회

--- src/main/java/com/himedia/projectteamdive/controller/MembershipController.java
+++ src/main/java/com/himedia/projectteamdive/controller/MembershipController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -16,11 +17,12 @@ public class MembershipController {
     @Autowired
     MembershipService mss;
 
-    /* 멤버십 리스트 가져오기 */
+    /* 카테고리에 해당하는 멤버십 정보 가져오기 */
     @GetMapping("/getMembership")
-    public HashMap<String, Object> getMembership() {
+    public HashMap<String, Object> getMembership(@RequestParam("category") String category) {
         HashMap<String, Object> result = new HashMap<>();
-        result.put("membershipList", mss.getMembership());
+        System.out.println("전달된 membership category : " + category);
+        result.put("membershipList", mss.getMembership(category));
         return result;
     }
 
