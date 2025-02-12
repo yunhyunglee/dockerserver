e91c6e4f6aac11f56959605dff5980ac9365dd57
naegangheo <hng8907@gmail.com>
2025-02-12 14:10:44 +0900

    fix: Security를 적용하여 로그인 알람에 오류가 있어서

--- src/main/java/com/himedia/projectteamdive/controller/MemberController.java
+++ src/main/java/com/himedia/projectteamdive/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.himedia.projectteamdive.entity.Member;
 import com.himedia.projectteamdive.security.util.CustomJWTException;
 import com.himedia.projectteamdive.security.util.JWTUtil;
 import com.himedia.projectteamdive.service.MemberService;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -24,27 +25,27 @@ public class MemberController {
         return "<h1>Welcome to MusicStreaming World</h1>";
     }
 
-    @PostMapping("/loginLocal")
-    public HashMap<String, Object> loginLocal(
-            @RequestParam("memberId") String memberId,
-            @RequestParam("password") String password,
-            HttpSession session){
-        HashMap<String, Object> result = new HashMap<>();
-        Member member = ms.getMember(memberId);
-        System.out.println(1);
-        if(member == null){
-            result.put("msg", "no");
-        }else if(!member.getPassword().equals(password)){
-            result.put("msg", "no");
-        }else{
-            System.out.println(2);
-            result.put("msg", "yes");
-            session.setAttribute("loginUser", member.getMemberId());
-            result.put("loginUser", member);
-        }
-        System.out.println(result);
-        return result;
-    }
+//    @PostMapping("/login")
+//    public HashMap<String, Object> loginLocal(
+//            @RequestParam("memberId") String memberId,
+//            @RequestParam("password") String password,
+//            HttpSession session){
+//        HashMap<String, Object> result = new HashMap<>();
+//        Member member = ms.getMember(memberId);
+//        System.out.println(1);
+//        if(member == null){
+//            result.put("msg", "no");
+//        }else if(!member.getPassword().equals(password)){
+//            result.put("msg", "no");
+//        }else{
+//            System.out.println(2);
+//            result.put("msg", "yes");
+//            session.setAttribute("loginUser", member.getMemberId());
+//            result.put("loginUser", member);
+//        }
+//        System.out.println(result);
+//        return result;
+//    }
 
     @GetMapping("/refresh")
     public HashMap<String, Object> refresh(
@@ -83,12 +84,12 @@ public class MemberController {
         return result;
     }
 
-    private boolean checkTime(Integer exp) {
-        java.util.Date expDate = new java.util.Date((long)exp*(1000));
-        long timeGap = expDate.getTime() - System.currentTimeMillis();
-        long remainTime = timeGap/(1000*60);
-        return remainTime < 60;
-    }
+        private boolean checkTime(Integer exp) {
+            java.util.Date expDate = new java.util.Date((long)exp*(1000));
+            long timeGap = expDate.getTime() - System.currentTimeMillis();
+            long remainTime = timeGap/(1000*60);
+            return remainTime < 60;
+        }
 
     private boolean checkExpiredToken(String accessToken) {
         try {
