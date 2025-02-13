4b167fe9f99b3775615367c67a4f9532fbdfbe1b
yunhyunglee <yunhyung91@gmail.com>
2025-02-13 12:49:48 +0900

    fix: 음악플레이어 재생시 db음악 재생수 증가 / 음악,앨범차트 조회

--- src/main/java/com/himedia/projectteamdive/entity/Playcountlist.java
+++ src/main/java/com/himedia/projectteamdive/entity/Playcountlist.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -29,11 +30,24 @@ public class Playcountlist {
     private Timestamp indate;  // 재생된 시간
 
     @Column(nullable = false)
-    private Integer playCount;  // 음악이 재생된 횟수
+    private Integer playCount=0;  // 음악이 재생된 횟수
 
     public Playcountlist(Music music, Timestamp indate, int playCount) {
         this.music = music;
         this.indate = indate;
         this.playCount = playCount;
+    }
+    @PrePersist
+    public void prePersist() {
+        if (this.indate != null) {
+            // Timestamp를 LocalDateTime으로 변환 후 시간 00:00으로 설정
+            LocalDateTime midnightLocalDateTime = indate.toLocalDateTime().toLocalDate().atStartOfDay();
+
+            // 다시 Timestamp로 변환
+            Timestamp midnightTimestamp = Timestamp.valueOf(midnightLocalDateTime);
+
+            // 00:00으로 설정된 Timestamp 저장
+            this.indate = midnightTimestamp;
+        }
     }
 }
--- src/main/java/com/himedia/projectteamdive/repository/MusicRepository.java
+++ src/main/java/com/himedia/projectteamdive/repository/MusicRepository.java
@@ -11,16 +11,16 @@ public interface MusicRepository extends JpaRepository<Music, Integer> {
 
     Music findByMusicId(int musicId);
 
-    @Modifying
-    @Query("update Music m set m.playCountDay=0")
-    void resetPlayCountDay();
-
-    @Modifying
-    @Query("update Music m set m.playCountWeek=0")
-    void resetPlayCountWeek();
-
-    @Modifying
-    @Query("update Music m set m.playCountMonth=0")
-    void resetPlayCountMonth();
+//    @Modifying
+//    @Query("update Music m set m.playCountDay=0")
+//    void resetPlayCountDay();
+//
+//    @Modifying
+//    @Query("update Music m set m.playCountWeek=0")
+//    void resetPlayCountWeek();
+//
+//    @Modifying
+//    @Query("update Music m set m.playCountMonth=0")
+//    void resetPlayCountMonth();
 
 }
