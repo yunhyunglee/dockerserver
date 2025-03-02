acb4d5cdf5686d030e37be26c9008515d82ed0cd
yunhyunglee <yunhyung91@gmail.com>
2025-03-02 19:09:34 +0900

    fix: client aws 완료

--- src/main/java/com/himedia/projectteamdive/controller/AIController.java
+++ src/main/java/com/himedia/projectteamdive/controller/AIController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 
 @RestController
-@RequestMapping("/AI")
+@RequestMapping("/api/AI")
 public class AIController {
 
     @Autowired
