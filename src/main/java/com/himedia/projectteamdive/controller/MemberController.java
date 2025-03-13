6a82b943a12776ce6b610626bed65c65aef38103
yunhyunglee <yunhyung91@gmail.com>
2025-03-13 11:54:24 +0900

    first commit

--- src/main/java/com/himedia/projectteamdive/controller/MemberController.java
+++ src/main/java/com/himedia/projectteamdive/controller/MemberController.java
@@ -37,7 +37,7 @@ public class MemberController {
 
     @GetMapping("/test")
     public String index(){
-        return "<h1>Welcome to M  222usicStreaming World</h1>";
+        return "<h1>Welcome to M222usicStreaming World</h1>";
     }
 
     @GetMapping("/refresh")
